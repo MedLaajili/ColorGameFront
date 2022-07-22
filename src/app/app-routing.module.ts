ef75ec8a9fa3bee6efb739ce4e5f5e387ed6cfbd
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-07-23 00:38:30 +0100

    Setting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginFormComponent} from "./user-auth/login-form/login-form.component";
+import {SignupFormComponent} from "./user-auth/signup-form/signup-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginFormComponent},
+  {path:'register',component: SignupFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
