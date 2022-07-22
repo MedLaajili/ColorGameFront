57aaa20dfda5a4212d99d004a6d3462b9a04a6c8
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-07-23 00:39:21 +0100

    login Form Component

--- src/app/user-auth/login-form/login-form.component.ts
+++ src/app/user-auth/login-form/login-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {UsersDataService} from '../../services/users-data.service'
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../user";
+import {UsersDataService} from "../../services/users-data.service";
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
@@ -9,24 +9,34 @@ import {User} from "../user";
 })
 export class LoginFormComponent implements OnInit {
   user:User = new User();
-  constructor(private userData:UsersDataService) {
+  showPassword: boolean = false;
 
-
-  }
+  constructor(private userData:UsersDataService) { }
 
   ngOnInit(): void {
   }
-  loginForm= new FormGroup({
-    email:new FormControl('',[Validators.required,Validators.email,Validators.minLength(6)]),
-    password:new FormControl('',[Validators.required,Validators.minLength(5)])
-});
+  loginForm=new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',Validators.required)
+  })
   onSubmit() {
-    // console.warn(data);
+    // console.log(data)
     this.user.username = this.loginForm.value.email;
-    this.user.password = this.loginForm.value.password
-    this.userData.saveUser(this.user).subscribe(user=>{
-      console.log(user)
-      alert("Successfully User is registered")
-    },error => alert("User is not registered"))
+    this.user.password = this.loginForm.value.password;
+    this.userData.saveUser(this.user).subscribe(data=>{
+      console.log(data)
+      // this.tokenStorage.saveToken(data.accessToken);
+      // this.tokenStorage.saveUser(data)
+      // this.reloadPage();
+    alert("Successfully Logged in, Welcome Back ")
+    },error => alert("Login failed"))
+  }
+
+  showHidePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  reloadPage(): void{
+    window.location.reload();
   }
 }
