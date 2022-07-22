f3e0f8c7fc76e12dccbc31df1183e1b530ea303a
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-07-23 00:40:56 +0100

    Ng Module

--- src/app/user-auth/user-auth.module.ts
+++ src/app/user-auth/user-auth.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {IonicModule} from "@ionic/angular";
+import { LoginFormComponent } from './login-form/login-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import "@angular/compiler"
 
 
 
 
 @NgModule({
     declarations: [
-    ],
+     LoginFormComponent
+  ],
     exports: [
+        LoginFormComponent
     ],
-    imports: [
-        CommonModule,
-    ]
+  imports: [
+    CommonModule,
+    IonicModule,
+    ReactiveFormsModule,
+  ]
 })
+@NgModule()
 export class UserAuthModule { }
